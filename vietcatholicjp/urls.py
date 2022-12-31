149f359581317e30bba42b4489ff5f9a0c54ebcb
danghoanggeo <dang.hoang.geo@gmail.com>
2023-01-01 01:33:28 +0900

    add home for admin

--- vietcatholicjp/urls.py
+++ vietcatholicjp/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+admin.site.site_header = 'Viet Catholic Japan'                    # default: "Django Administration"
+admin.site.index_title = 'Viet Catholic Japan Admintration'       # default: "Site administration"
+admin.site.site_title = 'Viet Catholic Japan Admintration'       # default: "Django site admin"
 
 urlpatterns = [
+    path('',include('home.urls')),
     path('admin/', admin.site.urls),
 ]
