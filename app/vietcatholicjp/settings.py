e8d5ea4ecaa7d86c310192c57fb102372a39c56f
danghoanggeo <dang.hoang.geo@gmail.com>
2023-01-01 00:08:38 +0900

    refector

--- app/vietcatholicjp/settings.py
+++ app/vietcatholicjp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eb!$sstrytjb!4)f!*5lu#bm25ub*4@l3o$9623x(++841gw$x'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-eb!$sstrytjb!4)f!*5lu#bm25ub*4@l3o$9623x(++841gw$x')
+JWT_SECRET_KEY= os.environ.get('JWT_SECRET_KEY','django-insecure-eb!$sstrytjb!4)f!*5lu#bm25ub*4@l3o$9623x(++841gw$x')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG','True') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
