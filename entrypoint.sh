f6938bf5d740dddc65f7c4192fcdff2556f7ef51
danghoanggeo <dang.hoang.geo@gmail.com>
2023-01-01 01:09:49 +0900

    update python v

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput
+python3.8 manage.py makemigrations --noinput
+python3.8 manage.py migrate --noinput
+python3.8 manage.py collectstatic --noinput
 # 環境変数のDEBUGの値がTrueの時はrunserverを、Falseの時はgunicornを実行します
 # シェルスクリプトでは`[`と`$DEBUG`、`1`と`]`の間にスペースを一つ空けておかないと[]内の式を認識できないので注意
 if [ $DEBUG = 1 ]; then
-    python3 manage.py runserver 0.0.0.0:8000
+    python3.8 manage.py runserver 0.0.0.0:8000
 else
     # gunicornを起動させる時はプロジェクト名を指定します
     # 今回はdjangopjにします
