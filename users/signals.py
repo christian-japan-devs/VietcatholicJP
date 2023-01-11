68df6190bd415522ff6a789407ef1e8c179e5754
danghoanggeo <dang.hoang.geo@gmail.com>
2023-01-11 11:58:33 +0900

    update user model

--- users/signals.py
+++ users/signals.py
@@ -3,9 +3,7 @@ from django.dispatch import receiver
 from .models import CustomUserModel
 from kanri.models import UserProfile
 
-'''
 @receiver(post_save, sender=CustomUserModel)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-'''
